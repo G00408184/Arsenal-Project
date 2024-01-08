99a336fcc86d0a3011e8d397d590fd177bcf745f
Jakub Mamala <g00408184@atu.ie>
2024-01-08 19:38:07 +0000

    fixed the player class had dependency errors

--- src/main/java/ie/atu/arsenalproject/Player.java
+++ src/main/java/ie/atu/arsenalproject/Player.java
@@ -1,18 +1,21 @@
 package ie.atu.arsenalproject;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 
 @Data
 @NoArgsConstructor
-
+@Entity
 public class Player {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Name cannot be blank")
@@ -31,5 +34,4 @@ public class Player {
 
     @NotBlank(message = "Position cannot be blank")
     private String position;
-
 }
